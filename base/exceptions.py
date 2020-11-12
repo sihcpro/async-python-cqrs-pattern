a41722e5c00a79c37871d04ace4f3db38a249ac7
sihc-PC <tvquocchi@gmail.com>
2020-11-12 21:55:23 +0700

    feat: print data to console when exception raised

--- base/exceptions.py
+++ base/exceptions.py
@@ -1,17 +1,28 @@
 from .response import AppResponse
+from app.cfg import logger
 
 
 class AppException(Exception):
     __status_code__ = 500
     __message__ = "Application Error"
+    __debug_info__ = True
 
     def __init__(self, errcode: int, message: str = None, data: dict = None):
         self.errcode = errcode
         self.data = data if data is not None else dict()
         self.message = message or self.__message__
-        self.resp = AppResponse(body=self.build_body(), status=self.__status_code__)
 
         super(AppException, self).__init__(self.message)
+        self.resp: AppResponse = AppResponse(
+            body=self.build_body(), status=self.__status_code__
+        )
+        self.notify()
+
+    def notify(self):
+        if not self.__debug_info__:
+            return
+        logger.error("[%d] %s\n%r", self.errcode, self.message, self.data)
+        logger.exception(self)
 
     def build_body(self) -> dict:
         return {
@@ -30,6 +41,7 @@ class BadRequestException(AppException):
 class UnauthorizedException(AppException):
     __status_code__ = 401
     __message__ = "UnauthorizedException"
+    __debug_info__ = False
 
 
 class ForbiddenException(AppException):
@@ -40,3 +52,4 @@ class ForbiddenException(AppException):
 class NotFoundException(AppException):
     __status_code__ = 404
     __message__ = "Not found item"
+    __debug_info__ = False
