76faed5b444cfe90b3a939c4ee77afbf28700828
sihc-PC <tvquocchi@gmail.com>
2021-01-18 22:01:35 +0700

    feat: get authentication key from cookie

--- auth/auth.py
+++ auth/auth.py
@@ -1,14 +1,27 @@
+import re
 from sanic.request import Request
 
 from base.exceptions import UnauthorizedException
-
 from .cfg import logger
 from .model import UserAuthModel, UserModel
 
 
 def user_auth(func):
+    cookie_re = re.compile(r"(\w*)=([^;]*)")
+    auth_keyword = "authorization"
+
+    def get_cookie(cookie):
+        if isinstance(cookie, dict):
+            return cookie.get(auth_keyword, "")
+        for match_cookie in cookie_re.findall(cookie):
+            if match_cookie[0] == auth_keyword:
+                return match_cookie[1]
+        return ""
+
     async def check_auth(request: Request, *args, **kwargs):
-        auth_header = request.headers.get("authorization", "")
+        auth_header = request.headers.get(auth_keyword, "") or get_cookie(
+            request.headers.get("cookie", {})
+        )
         if auth_header.startswith("Bearer"):
             auth_key = auth_header[7:]
             user = (
