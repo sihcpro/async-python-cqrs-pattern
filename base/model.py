428d567ecb4f55c7d3d1603613eae4f9b6e71e95
Sihc Zenbook <tvquocchi@gmail.com>
2021-06-18 06:48:07 +0700

    update: add default value into _created, _updated, _etag

--- base/model.py
+++ base/model.py
@@ -1,16 +1,17 @@
 import sqlalchemy
+from datetime import datetime
 from gino import Gino
 
-from .cfg import config
+from .hashes import generate_v1
 
 db: sqlalchemy = Gino()
 
 
 class Model(db.Model):
-    _created = db.Column(db.DateTime(timezone=False))
-    _updated = db.Column(db.DateTime(timezone=False))
+    _created = db.Column(db.DateTime(timezone=False), default=datetime.utcnow)
+    _updated = db.Column(db.DateTime(timezone=False), default=datetime.utcnow)
 
-    _etag = db.Column(db.String(255))
+    _etag = db.Column(db.String(255), default=generate_v1)
 
 
 __all__ = ("db", "Model")
